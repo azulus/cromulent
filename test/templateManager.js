c6a20439e15627a790f0e799bf19ede0215e4eb2
Jeremy Stanley <jeremy@obvious.com>
2012-07-02 03:01:27 -0700

    Updated templateManager tests to use multiple compiled template dirs

--- test/templateManager.js
+++ test/templateManager.js
@@ -3,16 +3,20 @@ var assert = require('assert')
 var TemplateManager = require("../lib/templateManager")
 
 var templates = new TemplateManager()
-templates.dirs.push(path.join(__dirname, 'templates'))
+templates.dirs.push(path.join(__dirname, 'templates/common'))
+templates.dirs.push(path.join(__dirname, 'templates/pages'))
 
 templates
   .compile()
-  .then(renderTemplate('test.simple.home', {name: "Jeremy"}))
+  .then(renderTemplate('common.pages.home', {name: "Jeremy"}))
   .then(validateOutput('<html><body>Hello, Jeremy!</body></html>'))
-  .then(renderTemplate('test.simple.header', {}))
+  .then(renderTemplate('common.pages.header', {}))
   .then(validateOutput('<html><body>'))
-  .then(renderTemplate('test.simple.footer', {}))
+  .then(renderTemplate('common.pages.footer', {}))
   .then(validateOutput('</body></html>'))
+  .fail(function (e) {
+    console.error(e)
+  })
 
 function renderTemplate(name, data) {
   return function () {
