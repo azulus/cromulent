e59784a5468121235ded7e20b4867082328d7fa7
Jeremy Stanley <jeremy@obvious.com>
2012-07-02 01:54:35 -0700

    First pass at a router

--- cromulent.js
+++ cromulent.js
@@ -1,1 +1,35 @@
-module.exports = 1
+module.exports = {
+	Server: require("./lib/server")
+}
+
+// lazy loading vs startup-time initialization
+
+// how does context work?
+
+// how does profiling work?
+
+// how does restarting work?
+
+// how does middleware work?
+
+// where are the graph nodes
+
+// where are the routes?
+
+// where are the templates?
+
+// where are the client-side js files?
+
+// where are the static files
+
+/**
+
+1) define the modules w/ the graph nodes
+
+2) define the routes which use those graph nodes
+
+3) define the views to render something sane out from the routes
+
+4) 
+
+*/
