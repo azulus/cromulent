6b7b0c584e6e47f8c8307f354ac13c088a894642
Jeremy Stanley <jeremy@obvious.com>
2012-07-06 20:28:38 -0700

    Renamed files and changed templates to use promises

---
+++ test/modules.js
@@ -0,0 +1,14 @@
+var path = require('path')
+var ModuleLoader = require('../lib/modules').ModuleLoader
+
+var loader = new ModuleLoader()
+loader.dirs.push(path.join(__dirname, 'modules'))
+
+loader
+.load()
+.then(function () {
+  console.log("done", arguments)
+})
+.fail(function (e) {
+  console.error(e)
+})
