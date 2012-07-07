bc867826c33321a771f2a6994f00fc56de1c739a
Jeremy Stanley <jeremy@obvious.com>
2012-07-06 22:54:08 -0700

    Updated module test to actually use the graph

--- test/modules/name.js
+++ test/modules/name.js
@@ -1,31 +1,17 @@
-module.exports = function (graph) {
+module.exports = function (moduleLoader) {
 
-  graph.registry.defineNode().outputs('firstName').with(function () {
-    var names = [
-      'Adam',
-      'Bob',
-      'Chris',
-      'David',
-      'Eric'
-    ]
-
-    return names[Math.floor(Math.random() * names.length)]
+  moduleLoader.registry.defineNode().given('first_names').outputs('first_name').with(function (firstNames) {
+    firstNames = firstNames.get()
+    return firstNames[Math.floor(Math.random() * firstNames.length)]
   }).build()
 
-  graph.registry.defineNode().outputs('lastName').with(function () {
-    var names = [
-      'Alexander',
-      'Barry',
-      'Cole',
-      'Diaz',
-      'Elwes'
-    ]
+  moduleLoader.registry.defineNode().given('last_names').outputs('last_name').with(function (lastNames) {
+    lastNames = lastNames.get()
+    return lastNames[Math.floor(Math.random() * lastNames.length)]
+  }).build()
 
-    return names[Math.floor(Math.random() * names.length)]
-  })
-
-  graph.registry.defineNode().given('firstName', 'lastName').with(function (firstName, lastName) {
-    return firstName + ' ' + lastName
-  })
+  moduleLoader.registry.defineNode().outputs('full_name').given('first_name', 'last_name').with(function (firstName, lastName) {
+    return firstName.get() + ' ' + lastName.get()
+  }).build()
 
 }
