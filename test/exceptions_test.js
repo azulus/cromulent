c417263860e435febfb13129b21451e7e80d9847
David Byttow <guitardave@gmail.com>
2012-07-07 14:47:52 -0700

    First pass at exception handling code.
    Conflicts:
    
    	test.js

---
+++ test/exceptions_test.js
@@ -0,0 +1,47 @@
+var path = require('path')
+  , exceptions = require('../lib/exceptions')
+  , ErrorResponse = exceptions.ErrorResponse
+  , util = require('util')
+  , TemplateManager = require('../lib/templates').TemplateManager
+
+
+// TODO(david): Move the generic request exceptions to a common location in the library.
+
+function BadRequest(message) {
+  BadRequest.super_.call(this, message)
+}
+util.inherits(BadRequest, Error)
+BadRequest.prototype.name = 'BadRequest'
+
+function NotFound() {
+  NotFound.super_.call(this)
+}
+util.inherits(NotFound, Error)
+NotFound.prototype.name = 'NotFound'
+
+function NotFound2() {}
+util.inherits(NotFound2, NotFound)
+NotFound2.prototype.name = 'NotFound2'
+
+
+// Register a couple exceptions.
+exceptions.registerHttpStatus(BadRequest, 400)
+
+exceptions.registerCustomHandler(NotFound, function (err) {
+  return new ErrorResponse(404, err.message)
+})
+
+
+function runTests() {
+  // TODO(david): Turn these into asserts.
+  console.log(exceptions.handle(new NotFound()))
+  console.log(exceptions.handle(new BadRequest('error')))
+  console.log(exceptions.handle(new Error('random error')))
+  console.log(exceptions.handle(new NotFound2()))
+}
+
+
+// Compile the error templates for testing.
+var templates = new TemplateManager()
+templates.dirs.push(path.join(__dirname, '../lib/client'))
+templates.compile().then(runTests).end()
