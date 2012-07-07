7605fa7180e5ff40285a1a9dea7e3341463b9a8c
David Byttow <guitardave@gmail.com>
2012-07-07 16:17:25 -0700

    Fixed modules_test

--- test/modules_test.js
+++ test/modules_test.js
@@ -4,15 +4,41 @@
 
 var path = require('path')
 var testCase = require('nodeunit').testCase
+
 var ModuleLoader = require('../lib/modules').ModuleLoader
+var Registry = require('composers').Registry
+var Scope = require('composers').Scope
+
+var registry
+var loader
 
 module.exports = testCase({
-  testLoading: function (test) {
-  	var loader = new ModuleLoader()
+  setUp: function (done) {
+    registry = new Registry()
+  	loader = new ModuleLoader(registry)
     loader.dirs.push(path.join(__dirname, 'modules'))
 
     loader.load().then(function () {
-      test.done()
+      done()
     }).end()
-  }, 
+  },
+
+  testNames: function (test) {
+    var firstNames = ['Adam','Bob','Chris','David','Eric']
+    var lastNames = ['Alexander','Barry','Cole','Diaz','Elwes']
+    var scope = new Scope(registry)
+    scope.enter()
+
+    return scope.createGraph('full_name')
+        .give('first_names', firstNames)
+        .give('last_names', lastNames)
+        .start()
+        .then(function (fullName) {
+          var nameParts = fullName.split(' ')
+          test.equal(firstNames.indexOf(nameParts[0]) !== -1, true)
+          test.equal(lastNames.indexOf(nameParts[1]) !== -1, true)
+          test.done()
+        })
+        .end()
+  }
 })
